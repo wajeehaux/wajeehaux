b3f8100fced5f1b0767fd1855dcda172ab93cca4
wajeehaux <wbatool1999@gmail.com>
2024-01-08 21:15:44 -0800

    Updates

---
+++ _app/immutable/chunks/paths.LDariDpa.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_187xtgy?.base??"/wajeehaux",a=globalThis.__sveltekit_187xtgy?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.rITIDmdG.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1nz1gi9?.base??"/wajeehaux",a=globalThis.__sveltekit_1nz1gi9?.assets??s;export{a,s as b};
